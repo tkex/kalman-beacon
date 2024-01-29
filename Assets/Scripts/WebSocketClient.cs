24c62ea6fcae459b0923d0213f2b1838f6209852
tkex <tikado@web.de>
2024-01-29 05:39:06 +0100

    feat: add boolean control for data flow and configured for measurement data

--- Assets/Scripts/WebSocketClient.cs
+++ Assets/Scripts/WebSocketClient.cs
@@ -1,14 +1,50 @@
 
 using UnityEngine;
 using NativeWebSocket;
+using System.Collections.Generic;
 
 public class WebSocketClient : MonoBehaviour
 {
     WebSocket websocket;
+    public Measurement measurementData;
+
+    // Reference.
+    public Sensor sensor;
+
+    // Slider from 0.1 to 5secs.
+    [Range(0.1f, 5.0f)]
+    public float sendInterval = 1.0f;
+
+    // Control vars to define which data are transmitted.
+    public bool sendBeaconFlag;
+    public bool sendTimestamp;
+    public bool sendBeaconId;
+    public bool sendBeaconPos;
+    public bool sendAngleGroundTruth;
+    public bool sendAngleDistorted;
+    public bool sendSensorSTD;
+    public bool sendHeadingAngleGroundTruth;
+    public bool sendHeadingAngleDistorted;
+    public bool sendCompassSTD;
+
+    // Control flag to only send data if measurement data are available. 
+    private bool isReadyToSend = false;
 
     // Start is called before the first frame update
     async void Start()
     {
+        // *** SENSOR EVENT CONFIGURATION ***
+        if (sensor == null)
+        {
+            Debug.LogError("IMPORTANT: Sensor is NOT assigned in WebSocketClient gameobject!");
+            return;
+        }
+
+        // Add Sensor Listener.
+        sensor.conductedMeasurement.AddListener(HandleNewMeasurement);
+
+
+        // *** WEBSOCKET EVENT CONFIGURATION ***
         websocket = new WebSocket("ws://localhost:2567");
 
         websocket.OnOpen += () =>
@@ -36,10 +72,10 @@ public class WebSocketClient : MonoBehaviour
             // Debug.Log("OnMessage! " + message);
         };
 
-        // Keep sending messages at every 0.3s
-        InvokeRepeating("SendWebSocketMessage", 0.0f, 0.3f);
+        // Keep sending messages dependend on configured sendInterval.
+        InvokeRepeating("SendWebSocketMessage", 0.0f, sendInterval);
 
-        // waiting for messages
+        // Waiting for messages
         await websocket.Connect();
     }
 
@@ -50,6 +86,54 @@ public class WebSocketClient : MonoBehaviour
 #endif
     }
 
+  
+    string SendDataBlock()
+    {
+        List<string> filterDataParts = new List<string>();
+
+        // Add individual measureData.
+        if (sendTimestamp) filterDataParts.Add(measurementData.timestamp.ToString());
+        if (sendBeaconId) filterDataParts.Add(measurementData.beaconId.ToString());
+        if (sendBeaconPos) filterDataParts.Add(measurementData.beaconPos.ToString());
+        if (sendAngleGroundTruth) filterDataParts.Add(measurementData.angleGroundTruth.ToString());
+        if (sendAngleDistorted) filterDataParts.Add(measurementData.angleDistorted.ToString());
+        if (sendSensorSTD) filterDataParts.Add(measurementData.sensorSTD.ToString());
+        if (sendHeadingAngleGroundTruth) filterDataParts.Add(measurementData.headingAngleGroundTruth.ToString());
+        if (sendHeadingAngleDistorted) filterDataParts.Add(measurementData.headingAngleDistorted.ToString());
+        if (sendCompassSTD) filterDataParts.Add(measurementData.compassSTD.ToString());
+        if (sendBeaconFlag) filterDataParts.Add(measurementData.beaconFlag.ToString());
+
+        // Space data via tabs.
+        return string.Join("\t", filterDataParts);
+    }
+
+    private void HandleNewMeasurement(Measurement measurement)
+    {
+        // Refresh/update measurementData with new fetched data.
+        measurementData = measurement;
+
+        // Send new msg with new data.
+        //SendWebSocketMessage();
+
+        if (!isReadyToSend)
+        {
+            isReadyToSend = true;
+
+            InvokeRepeating("SendWebSocketMessage", 0.0f, sendInterval);
+        }
+    }
+
+    async void SendWebSocketMessage()
+    {
+        if (websocket.State == WebSocketState.Open && isReadyToSend && measurementData != null)
+        {
+            string message = SendDataBlock();
+
+            await websocket.SendText(message);
+        }
+    }
+
+    /*
     async void SendWebSocketMessage()
     {
         if (websocket.State == WebSocketState.Open)
@@ -61,6 +145,8 @@ public class WebSocketClient : MonoBehaviour
             await websocket.SendText("plain text message");
         }
     }
+    */
+
 
     private async void OnApplicationQuit()
     {
