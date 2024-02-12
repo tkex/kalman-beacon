6ab4b56c1d9f46caf6d0a6556dacdad0c6f81d75
tkex <tikado@web.de>
2024-02-12 17:41:06 +0100

    feat: add proper distance measurement in unity and adjust log data

--- Assets/Scripts/MessSensor.cs
+++ Assets/Scripts/MessSensor.cs
@@ -31,10 +31,10 @@ public class Messsensor: MonoBehaviour
     // Directions (werden aus angles berechnet)
     private Vector2[] beaconDirections = new Vector2[3]; // in ships local modelspace  
     private Vector2[] beaconDirectionsDistorted = new Vector2[3]; // in ships local modelspace  
-    
+
     // Entfernung (skalierte directions)
-    private Vector2[] beaconEntfernungen = new Vector2[3];
-    private Vector2[] beaconEntfernungenDistorted = new Vector2[3];
+    private float[] beaconEntfernungen = new float[3];
+    private float[] beaconEntfernungenDistorted = new float[3];
 
     public float[] beaconStds = new float[3]{1, 1, 1}; // verrauscht sämtliche Messdaten für jeweilges Beacon
     
@@ -113,19 +113,26 @@ public class Messsensor: MonoBehaviour
 
     void CalcAndSetEntfernungen()
     {
-      // FRAGE: ist das fine so? (nachträgliche Skalierung)
-      float distanceBeacon0 = (beacons[0].transform.position - transform.position).magnitude;
-      float distanceBeacon1 = (beacons[1].transform.position - transform.position).magnitude;
-      float distanceBeacon2 = (beacons[2].transform.position - transform.position).magnitude;
-      
-      // GT
-      beaconEntfernungen[0] = beaconDirections[0] * distanceBeacon0;
-      beaconEntfernungen[1] = beaconDirections[1] * distanceBeacon1;
-      beaconEntfernungen[2] = beaconDirections[2] * distanceBeacon2;
-      // Distorted 
-      beaconEntfernungenDistorted[0] = beaconDirectionsDistorted[0] * distanceBeacon0;
-      beaconEntfernungenDistorted[1] = beaconDirectionsDistorted[1] * distanceBeacon1;
-      beaconEntfernungenDistorted[2] = beaconDirectionsDistorted[2] * distanceBeacon2;
+        // Berechnung der direkten Entfernungen zu den Beacons
+        float distanceBeacon0 = Vector3.Distance(beacons[0].transform.position, transform.position);
+        float distanceBeacon1 = Vector3.Distance(beacons[1].transform.position, transform.position);
+        float distanceBeacon2 = Vector3.Distance(beacons[2].transform.position, transform.position);
+
+        // Debug Log
+        Debug.Log($"Distanz zu Beacon 0: {distanceBeacon0}");
+        Debug.Log($"Distanz zu Beacon 1: {distanceBeacon1}");
+        Debug.Log($"Distanz zu Beacon 2: {distanceBeacon2}");
+
+
+        // Speichern der direkten Entfernungen (ohne Richtungsberechnung)
+        beaconEntfernungen[0] = distanceBeacon0; // Direkte Entfernung zu Beacon 0
+        beaconEntfernungen[1] = distanceBeacon1; // Direkte Entfernung zu Beacon 1
+        beaconEntfernungen[2] = distanceBeacon2; // Direkte Entfernung zu Beacon 2
+
+        // Verrauschte Entfernungen
+        beaconEntfernungenDistorted[0] = distanceBeacon0 + GenerateRandomGaussian(0, beaconStds[0]);
+        beaconEntfernungenDistorted[1] = distanceBeacon1 + GenerateRandomGaussian(0, beaconStds[1]);
+        beaconEntfernungenDistorted[2] = distanceBeacon2 + GenerateRandomGaussian(0, beaconStds[2]);
     }
 
 
@@ -186,32 +193,37 @@ public class Messsensor: MonoBehaviour
     /*
     Extended log format:
 
+    -- Zeit
     [0]  'Zeit_Index',
-    -- position
+
+    -- Position
     [1]  'X_GT',  (X-Koordinate Ground Truth)
     [2]  'Y_GT',  (Y-Koordinate Ground Truth)
-    -- heading
+    
+    -- Heading
     [3]  'Heading_GT'  (Heading - Winkeländerung zu Ausgangsposition Norden)
     [4]  'Heading'  (Heading distorted)
     [5]  'Heading_STD' (STD für Heading)
-    -- angles 
+    
+    -- Angles 
     [6]  'angle_GT_B0', 'angleDistorted_B0', 'STD_Grad_B0',  (Daten Beacon 0)
     [9]  'angle_GT_B1', 'angleDistorted_B1', 'STD_Grad_B1',  (Daten Beacon 1)
     [12] 'angle_GT_B2', 'angleDistorted_B2', 'STD_Grad_B2',  (Daten Beacon 2)
-    -- directions
+
+    -- Directions
     [13] 'Richtung_GT_X_B0', 'Richtung_GT_Y_B0',
     [15] 'Richtung_X_B0', 'Richtung_Y_B0', 'STD_Grad_B0'
     [18] 'Richtung_GT_X_B1', 'Richtung_GT_Y_B1', 
     [20] 'Richtung_X_B1', 'Richtung_Y_B1', 'STD_Grad_B1'
     [23] 'Richtung_GT_X_B2', 'Richtung_GT_Y_B2',
-    [25] 'Richtung_X_B2', 'Richtung_Y_B1', 'STD_Grad_B2'
-    -- entfernungen 
-    [28] 'Entfernung_GT_X_B0', 'Entfernung_GT_Y_B0',
-    [30] 'Entfernung_X_B0', 'Entfernung_Y_B0', 'STD_Grad_B0'
-    [33] 'Entfernung_GT_X_B1', 'Entfernung_GT_Y_B1', 
-    [35] 'Entfernung_X_B1', 'Entfernung_Y_B1', 'STD_Grad_B1'
-    [38] 'Entfernung_GT_X_B2', 'Entfernung_GT_Y_B2',
-    [40] 'Entfernung_X_B2', 'Entfernung_Y_B1', 'STD_Grad_B2'
+    [25] 'Richtung_X_B2', 'Richtung_Y_B1', 'STD_Grad_B2' 
+    
+    -- Absolute Entfernungen 
+    Direct distances to the beacons from the ship, noisy added distance and std
+    [28] 'Entfernung_B0', 'EntfernungDistorted_B0', 'STD_Entfernung_B0',
+    [31] 'Entfernung_B1', 'EntfernungDistorted_B1', 'STD_Entfernung_B1',
+    [34] 'Entfernung_B2', 'EntfernungDistorted_B2', 'STD_Entfernung_B2'
+
     */
 
     void WriteExtendedLog()
@@ -219,14 +231,18 @@ public class Messsensor: MonoBehaviour
         // TODO: runde auf 3 Dezimalstellen! 
         // TODO: setze indixes händisch um Fehler zu vermeiden
         string logText = $"{totalMeasurements}";
+
         // position
         logText += $"\t{transform.position.x}\t{transform.position.y}";
+
         // heading
         logText += $"\t{heading}\t{headingDistorted}\t{headingSTD}";
+
         // angles
         logText += $"\t{beaconAngles[0]}\t{beaconAnglesDistorted[0]}\t{beaconStds[0]}";
         logText += $"\t{beaconAngles[1]}\t{beaconAnglesDistorted[1]}\t{beaconStds[1]}";
         logText += $"\t{beaconAngles[2]}\t{beaconAnglesDistorted[2]}\t{beaconStds[2]}";
+
         // directions
         logText += $"\t{beaconDirections[0].x}\t{beaconDirections[0].y}";
         logText += $"\t{beaconDirectionsDistorted[0].x}\t{beaconDirectionsDistorted[0].y}\t{beaconStds[0]}";
@@ -234,13 +250,11 @@ public class Messsensor: MonoBehaviour
         logText += $"\t{beaconDirectionsDistorted[1].x}\t{beaconDirectionsDistorted[1].y}\t{beaconStds[1]}";
         logText += $"\t{beaconDirections[2].x}\t{beaconDirections[2].y}";
         logText += $"\t{beaconDirectionsDistorted[2].x}\t{beaconDirectionsDistorted[2].y}\t{beaconStds[2]}";
-        // entfernungen
-        logText += $"\t{beaconEntfernungen[0].x}\t{beaconEntfernungen[0].y}";
-        logText += $"\t{beaconEntfernungenDistorted[0].x}\t{beaconEntfernungenDistorted[0].y}\t{beaconStds[0]}";
-        logText += $"\t{beaconEntfernungen[1].x}\t{beaconEntfernungen[1].y}";
-        logText += $"\t{beaconEntfernungenDistorted[1].x}\t{beaconEntfernungenDistorted[1].y}\t{beaconStds[1]}";
-        logText += $"\t{beaconEntfernungen[2].x}\t{beaconEntfernungen[2].y}";
-        logText += $"\t{beaconEntfernungenDistorted[2].x}\t{beaconEntfernungenDistorted[2].y}\t{beaconStds[2]}";
+      
+        // absolute distances to the beacons
+        logText += $"\t{beaconEntfernungen[0]}\t{beaconEntfernungenDistorted[0]}\t{beaconStds[0]}";
+        logText += $"\t{beaconEntfernungen[1]}\t{beaconEntfernungenDistorted[1]}\t{beaconStds[1]}";
+        logText += $"\t{beaconEntfernungen[2]}\t{beaconEntfernungenDistorted[2]}\t{beaconStds[2]}";
 
         logText = logText.Replace(',', '.');
 
